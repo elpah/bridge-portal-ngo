5aa0b8808e5c8d3be026e3a0e2a09596647262a8
Elpah <obengelpachris@gmail.com>
2025-06-04 05:08:12 +0200

    leave comment form layout'

--- src/components/leave-comment/LeaveComment.tsx
+++ src/components/leave-comment/LeaveComment.tsx
@@ -1,9 +1,20 @@
-import styles from "./leave-comment.module.css"
+import { HiOutlineArrowRight } from "react-icons/hi";
+import styles from "./leave-comment.module.css";
 
 const LeaveComment = () => {
   return (
-	<section className={styles.leave_comment_container}>LeaveComment</section>
-  )
-}
+    <form className={styles.leave_comment_container}>
+      <input type="text" placeholder="Your Name" />
+      <div className={styles.email_position_container}>
+        <input type="text" placeholder="Your Email" />
+        <input type="text" placeholder="Your Position" />
+      </div>
+	  <textarea name="" id="" placeholder="Leave a comment"></textarea>
+	  <button>
+		Submit Your Comment{" "}<HiOutlineArrowRight className={styles.arrow}/>
+	  </button>
+    </form>
+  );
+};
 
-export default LeaveComment
+export default LeaveComment;
